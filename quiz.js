16e6a977323bc906c045024b7f36801384ffdd10
LouisJsummers <louisummers@yahoo.co.uk>
2023-04-26 20:07:11 +0100

    set up file stucture

---
+++ quiz.js
@@ -0,0 +1,23 @@
+var startQuiz = document.getElementById(startQuiz);
+var saveScore = document.getElementById(saveScore);
+var viewScores = document.getElementById(viewScores);
+var playAgain = document.getElementById(playAgain);
+
+
+
+var welcome = document.getElementById(welcome);
+var quiz = document.getElementById(quiz);
+var result = document.getElementById(result);
+
+var options = document.getElementById(options);
+var message = document.getElementById(message);
+
+var timer = document.getElementById(timer);
+
+var summary = document.getElementById(summary);
+
+var secondsLeft = 0;
+var score = 0;
+var currentQuestion = 0;
+var countDownTimer;
+
